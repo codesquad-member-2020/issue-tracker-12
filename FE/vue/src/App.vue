c65f81058bfd4a5d2d71778b407f6a8c2162655c
Hu2y <sanghun_@daum.net>
2020-06-09 17:31:24 +0900

    [#10] Feat: vue 인스턴스에 라우트 객체 주입 및 root App에 router-view 연결

--- FE/vue/src/App.vue
+++ FE/vue/src/App.vue
@@ -1,15 +1,11 @@
 <template>
   <div id="app">
-    test
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
