c8c3a86f91a105ef155b1eecc319195f2919ea05
wooody92 <pkower4@naver.com>
2020-06-09 16:29:18 +0900

    [#7] chore : change depth from 2 to 4
    - 들여쓰기 설정값 변경헀습니다.

--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/common/exception/ExceptionAdvice.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/common/exception/ExceptionAdvice.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionAdvice {
 
-  @ExceptionHandler
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  private String catchInvalidTokenException(InvalidTokenException e) {
-    return e.getMessage();
-  }
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    private String catchInvalidTokenException(InvalidTokenException e) {
+        return e.getMessage();
+    }
 }
--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/common/exception/InvalidTokenException.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/common/exception/InvalidTokenException.java
@@ -2,17 +2,17 @@ package dev.codesquad.issuetracker.common.exception;
 
 public class InvalidTokenException extends RuntimeException {
 
-  public InvalidTokenException() {}
+    public InvalidTokenException() {}
 
-  public InvalidTokenException(String message) {
-    super(message);
-  }
+    public InvalidTokenException(String message) {
+        super(message);
+    }
 
-  public InvalidTokenException(String message, Throwable cause) {
-    super(message, cause);
-  }
+    public InvalidTokenException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
-  public InvalidTokenException(Throwable cause) {
-    super(cause);
-  }
+    public InvalidTokenException(Throwable cause) {
+        super(cause);
+    }
 }
