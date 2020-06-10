6fd4f75fb7bfa5b3fed2c9ddb72de73f5a3bc8bd
Hu2y <sanghun_@daum.net>
2020-06-10 16:59:51 +0900

    [#14] Feat: store labelCount 상태 추가

---
+++ FE/vue/src/store/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    labelCount: 0,
+  },
+  mutations: {},
+});
+
+export default store;
