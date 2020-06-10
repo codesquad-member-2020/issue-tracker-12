befbfcfe52d9a9d391cdf06de6b6be1f9416d67e
wooody92 <pkower4@naver.com>
2020-06-11 02:53:23 +0900

    [#19] feat : create IssueRepository
    - entity manager가 관리하는 repo 생성헀습니다.

---
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/repository/IssueRepository.java
@@ -0,0 +1,26 @@
+package dev.codesquad.issuetracker.repository;
+
+import dev.codesquad.issuetracker.domain.issue.Issue;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class IssueRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Issue issue) {
+        em.persist(issue);
+    }
+
+    public Issue findOne(Long id) {
+        return em.find(Issue.class, id);
+    }
+
+    public List<Issue> findAll() {
+        return em.createQuery("select i from Issue i", Issue.class).getResultList();
+    }
+}
