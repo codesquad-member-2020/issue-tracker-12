7fc5acdc90c725cdc9a914576e1b58a6cafcfa21
sangwon21 <junothyme@gmail.com>
2020-06-10 11:34:22 +0900

    [FE #15] feat: 레이아웃용 컴포넌트 작성
    why: 레이아웃용 컴포넌트를 작성했다. 이는, 레이아웃을 잡을 때, 계속해서
    반복적으로 쓰이는 CSS Flex를, 한 컴포넌트에서 관리하기 위함이다. 이를
    통해, 컴포넌트 스타일링 과정에서, 일일이 flex를 할 필요가 없게 되었다.

---
+++ FE/react/src/stories/Flex.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Flex from '@Custom/Flex/Flex';
+import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
+import Icon from '@Icon/Icon';
+import { action } from '@storybook/addon-actions';
+
+export default {
+  title: 'components|Flex',
+  component: Flex,
+  decorators: [withKnobs],
+  parameters: {
+    componentSubtitle: 'Issue Tracker에서 사용할 버튼 컴포넌트'
+  }
+};
+
+export const flex = () => {
+  return (
+    <Flex gap={10}>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+    </Flex>
+  );
+};
+
+export const flexDefault = () => {
+  const direction = select('direction', ['column', 'row'], 'row');
+  const align = select('align', ['left', 'right', 'spaceAround', 'spaceBetween'], 'left');
+  const wrap = boolean('wrap', false);
+  const gap = text('gap', '5px');
+  const width = text('width', '100%');
+  const alignItemCenter = boolean('alignItemCenter', false);
+  return (
+    <Flex direction={direction} align={align} wrap={wrap} gap={gap} width={width} alignItemCenter={alignItemCenter}>
+      <div>아</div>
+      <div>이</div>
+      <div>앤</div>
+      <div>잴</div>
+    </Flex>
+  );
+};
+
+flexDefault.story = {
+  name: 'Default'
+};
