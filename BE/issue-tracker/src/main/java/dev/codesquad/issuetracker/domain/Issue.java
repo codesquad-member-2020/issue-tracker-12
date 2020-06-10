9224ca7a8c0ef861e631e3007c5f14a5318a7418
wooody92 <pkower4@naver.com>
2020-06-11 00:28:06 +0900

    [#7] feat : add 1:M relation on user:issue
    - user : issue 1:N으로 연관관계 설정했습니다.
    - M:N으로도 맵핑하여 같은 엔티티의 두개의 리스트 형식이 생겼는데 처리방식이 맞는지 의문입니다.

--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/Issue.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/Issue.java
@@ -6,12 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
@@ -20,7 +22,7 @@ import lombok.ToString;
 
 @Entity
 @Getter
-@ToString(exclude = "users")
+@ToString(exclude = {"users", "user"})
 @NoArgsConstructor
 public class Issue {
 
@@ -49,4 +51,9 @@ public class Issue {
         joinColumns = @JoinColumn(name = "issue_id"),
         inverseJoinColumns = @JoinColumn(name = "user_id"))
     private List<User> users = new ArrayList<>();
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/User.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/User.java
@@ -2,12 +2,11 @@ package dev.codesquad.issuetracker.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
@@ -35,4 +34,7 @@ public class User {
 
     @ManyToMany(mappedBy = "users")
     private List<Issue> issues = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Issue> issueList = new ArrayList<>();
 }
