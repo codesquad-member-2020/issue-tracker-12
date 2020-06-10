9ca206c8c54f15041d22ed7949dfadbfb2f91df0
wooody92 <pkower4@naver.com>
2020-06-11 00:07:15 +0900

    [#7] feat : let many-to-many relataion on issue - user entity(assignee)
    - issue - user M:N 맵핑으로 assignee 테이블 생성

--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/Issue.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/Issue.java
@@ -1,5 +1,6 @@
 package dev.codesquad.issuetracker.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +9,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
@@ -16,7 +20,7 @@ import lombok.ToString;
 
 @Entity
 @Getter
-@ToString
+@ToString(exclude = "users")
 @NoArgsConstructor
 public class Issue {
 
@@ -38,4 +42,11 @@ public class Issue {
 
     @OneToMany(mappedBy = "issue", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(name = "assignee",
+        joinColumns = @JoinColumn(name = "issue_id"),
+        inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users = new ArrayList<>();
 }
--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/User.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/User.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
@@ -29,4 +32,7 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private List<Comment> comments = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "users")
+    private List<Issue> issues = new ArrayList<>();
 }
