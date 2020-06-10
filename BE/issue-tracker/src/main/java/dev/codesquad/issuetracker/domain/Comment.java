78022edbb451dd9cd8fa10f701a991633c635a13
wooody92 <pkower4@naver.com>
2020-06-10 20:00:43 +0900

    [#7] feat : mapping user-comment entity to one-to-many relation
    - 연관관계 테이블 1:N 맵핑했습니다.

--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/Comment.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/Comment.java
@@ -1,11 +1,15 @@
 package dev.codesquad.issuetracker.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,22 +17,20 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
-public class Issue {
+public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
-    private String title;
-
-    @NotNull
     private String content;
-
-    @NotNull
-    private boolean status;
 
     @NotNull
     private LocalDate create_time;
 
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/User.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/User.java
@@ -1,10 +1,13 @@
 package dev.codesquad.issuetracker.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,11 +19,13 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
     private Long id;
 
     @NotNull
     private String githubId;
 
     private String profileUrl;
+
+    @OneToMany(mappedBy = "user")
+    private List<Comment> comments = new ArrayList<>();
 }
