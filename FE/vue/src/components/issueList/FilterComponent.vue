5fa134112750b25eed64ef90f054535e000f4dcf
Hu2y <sanghun_@daum.net>
2020-06-10 16:58:37 +0900

    [#14] Chore: Filter 컴포넌트 스켈레톤 작성

---
+++ FE/vue/src/components/issueList/FilterComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <select v-model="selected">
+      <option class="default-filter-title" disabled value=""
+        >Filter Issues</option
+      >
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+    <span>선택함: {{ selected }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selected: '',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.default-filter-title {
+  color: red;
+}
+</style>
