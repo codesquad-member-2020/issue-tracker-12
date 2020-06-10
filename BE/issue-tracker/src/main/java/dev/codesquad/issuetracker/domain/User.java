4576c47f2953941bce5630522513ba98454668da
wooody92 <pkower4@naver.com>
2020-06-10 19:13:04 +0900

    [#7] feat : add domain-user entity class
    - user 엔티티 생성했습니다.

---
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/User.java
@@ -0,0 +1,26 @@
+package dev.codesquad.issuetracker.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @NotNull
+    private String githubId;
+
+    private String profileUrl;
+}
