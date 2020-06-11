a53aec6051bd8924f4679285969d45e30c4c6b83
wooody92 <pkower4@naver.com>
2020-06-11 20:27:45 +0900

    [#17] refactor : change label from v.o to entity
    - label을 vo -> entity로 변경했습니다.

--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/vo/Label.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/vo/Label.java
@@ -1,17 +1,39 @@
 package dev.codesquad.issuetracker.domain.vo;
 
-import javax.persistence.Embeddable;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.codesquad.issuetracker.domain.issue.Issue;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Embeddable
+@Entity
 @Getter
-@ToString
+@ToString(exclude = "issues")
 @NoArgsConstructor
 public class Label {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
     private String name;
+
+    @NotNull
     private String description;
+
+    @NotNull
     private String colorCode;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "labels")
+    private List<Issue> issues = new ArrayList<>();
 }
