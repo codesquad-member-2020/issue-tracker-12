8e474e08a33ccafe5c273d54ab4742097bc4c927
Hu2y <sanghun_@daum.net>
2020-06-11 12:44:05 +0900

    [#21] Feat: 레이블 목록화면 버튼과 인풋 컴포넌트 추가

--- FE/vue/src/views/LabelListPage.vue
+++ FE/vue/src/views/LabelListPage.vue
@@ -1,9 +1,49 @@
 <template>
-  <div>레이블 목록 페이지</div>
+  <div class="label-list-container">
+    <div class="label-header-container">
+      <LabelsButtonComponent :theme="baseStyles" />
+      <MileStoneButtonComponent />
+      <InputComponent />
+    </div>
+    <div>
+      컨텐츠 영역
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import LabelsButtonComponent from '@/components/issueList/LabelsButtonComponent';
+import MileStoneButtonComponent from '@/components/issueList/MileStoneButtonComponent';
+import InputComponent from '@/components/input/InputComponent';
+
+export default {
+  data() {
+    return {
+      baseStyles: {
+        color: '#fff',
+        backgroundColor: '#0366d6',
+        borderColor: '#0366d6',
+        float: 'left',
+        padding: '6px 14px',
+        fontWeight: '600',
+        lineHeight: '20px',
+      },
+    };
+  },
+  components: {
+    LabelsButtonComponent,
+    MileStoneButtonComponent,
+    InputComponent,
+  },
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.label-list-container {
+  float: left;
+}
+
+.label-header-container {
+  display: flex;
+}
+</style>
