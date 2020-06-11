e9bff25de61f9346bdda4ff5bc5cd96af916eeef
wooody92 <pkower4@naver.com>
2020-06-11 22:58:15 +0900

    [#19] feat : add issue status column on issue-response
    - 이슈 응답 객체에 status 상태값 추가했습니다.

--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/web/dto/IssueResponse.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/web/dto/IssueResponse.java
@@ -1,5 +1,7 @@
 package dev.codesquad.issuetracker.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.codesquad.issuetracker.domain.Status;
 import dev.codesquad.issuetracker.domain.issue.Issue;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -11,30 +13,33 @@ import lombok.Data;
 @Data
 public class IssueResponse {
 
-    private Long issueId;
+    private Long id;
     private String title;
     private String githubId;
     private LocalDate createTime;
+    private Status status;
     private int comment;
     private List<LabelDto> labels = new ArrayList<>();
 
     @Builder
-    protected IssueResponse(Long issueId, String title, String githubId,
-        LocalDate createTime, int comment, List<LabelDto> labels) {
-        this.issueId = issueId;
+    protected IssueResponse(Long id, String title, String githubId,
+        LocalDate createTime, Status status, int comment, List<LabelDto> labels) {
+        this.id = id;
         this.title = title;
         this.githubId = githubId;
         this.createTime = createTime;
+        this.status = status;
         this.comment = comment;
         this.labels = labels;
     }
 
     public static IssueResponse of(Issue issue) {
         return IssueResponse.builder()
-            .issueId(issue.getId())
+            .id(issue.getId())
             .title(issue.getTitle())
             .githubId(issue.getUser().getGithubId())
             .createTime(issue.getCreate_time())
+            .status(issue.getStatus())
             .comment(issue.getComments().size())
             .labels(issue.getLabels().stream()
                 .map(label -> LabelDto.of(label.getName(), label.getColorCode()))
