c5066d08ee45834ddd69d7f2da32ee4b747c6d2d
wooody92 <pkower4@naver.com>
2020-06-11 20:28:36 +0900

    [#17] refactor : change issue - label relation to many-to-many
    - 테이블 구조 변경으로 M:N 관계로 변경했습니다.

--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/issue/Issue.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/domain/issue/Issue.java
@@ -8,8 +8,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
-import javax.persistence.CollectionTable;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -70,8 +68,10 @@ public class Issue {
     @JoinColumn(name = "milestone_id")
     private Milestone milestone;
 
-    @ElementCollection
-    @CollectionTable(name = "label", joinColumns = @JoinColumn(name = "issue_id"))
+    @ManyToMany
+    @JoinTable(name = "issue_has_label",
+        joinColumns = @JoinColumn(name = "issue_id"),
+        inverseJoinColumns = @JoinColumn(name = "label_id"))
     private List<Label> labels = new ArrayList<>();
 
     @Builder
