dfa384a8ed03488b343c20807c583e33b58744af
wooody92 <pkower4@naver.com>
2020-06-11 20:38:18 +0900

    [#19] feat : add viweAll method which shows issue list
    - 이슈 목록을 보여주는 화면에 필요한 데이터들을 관리하는 메서드 입니다.

--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/service/IssueService.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/service/IssueService.java
@@ -1,7 +1,13 @@
 package dev.codesquad.issuetracker.service;
 
+import dev.codesquad.issuetracker.domain.vo.Label;
 import dev.codesquad.issuetracker.repository.IssueRepository;
-import dev.codesquad.issuetracker.web.dto.IssueListResponse;
+import dev.codesquad.issuetracker.repository.LabelRepository;
+import dev.codesquad.issuetracker.repository.UserRepository;
+import dev.codesquad.issuetracker.web.dto.IssueResponse;
+import dev.codesquad.issuetracker.web.dto.ResultResponse;
+import dev.codesquad.issuetracker.web.dto.ResultDto;
+import dev.codesquad.issuetracker.web.dto.UserResponse;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +19,35 @@ import org.springframework.transaction.annotation.Transactional;
 public class IssueService {
 
     private final IssueRepository issueRepository;
+    private final UserRepository userRepository;
+    private final LabelRepository labelRepository;
 
     @Transactional(readOnly = true)
-    public List<IssueListResponse> viewAll() {
+    public List<IssueResponse> viewAllIssue() {
         return issueRepository.findAllWithUserLabel().stream()
-            .map(issue -> IssueListResponse.of(issue))
+            .map(issue -> IssueResponse.of(issue))
             .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public ResultResponse viewAll() {
+        List<UserResponse> userResponses = userRepository.findAll().stream()
+            .map(user -> UserResponse.of(user))
+            .collect(Collectors.toList());
+        ResultDto user = new ResultDto(userResponses.size(), userResponses);
+
+        List<IssueResponse> issueResponses = issueRepository.findAllWithUserLabel().stream()
+            .map(issue -> IssueResponse.of(issue))
+            .collect(Collectors.toList());
+        ResultDto issue = new ResultDto(issueResponses.size(), issueResponses);
+
+        List<Label> labels = labelRepository.findAll();
+        ResultDto label = new ResultDto(labels.size(), labels);
+
+        return ResultResponse.builder()
+            .issue(issue)
+            .user(user)
+            .label(label)
+            .build();
     }
 }
