fb11f4ae345c9682bbb36d69bed42e5cdd4a66a6
Hu2y <sanghun_@daum.net>
2020-06-13 03:44:59 +0900

    [#21] Feat: label 추가 후 input값 초기화

--- FE/vue/src/components/issue/label/AddLabel.vue
+++ FE/vue/src/components/issue/label/AddLabel.vue
@@ -122,6 +122,8 @@ export default {
         description: this.labelDescription,
         labelName: this.labelName,
       });
+      this.labelName = '';
+      this.labelDescription = '';
     },
   },
 };
