cb8b3dab5e920867acabb340e2643f3040fe33a6
Hu2y <sanghun_@daum.net>
2020-06-13 01:55:40 +0900

    [#21] Feat: Label Item 컴포넌트 생성

---
+++ FE/vue/src/components/issue/label/Label.vue
@@ -0,0 +1,46 @@
+<template>
+  <LabelItem>
+    <Info>
+      <LabelTab>
+        <LabelPreview :label="label">
+          {{ label.labelName }}
+        </LabelPreview>
+      </LabelTab>
+      <DescriptionTab>{{ label.description }}</DescriptionTab>
+      <MutedLinkTab></MutedLinkTab>
+      <ButtonTab>
+        <Button>Edit</Button>
+        <Button>Delete</Button>
+      </ButtonTab>
+    </Info>
+  </LabelItem>
+</template>
+
+<script>
+import {
+  LabelItem,
+  LabelTab,
+  Info,
+  DescriptionTab,
+  MutedLinkTab,
+  ButtonTab,
+  Button,
+  LabelPreview,
+} from '@/style/styled';
+
+export default {
+  props: ['label'],
+  components: {
+    LabelItem,
+    LabelTab,
+    LabelPreview,
+    Info,
+    DescriptionTab,
+    MutedLinkTab,
+    ButtonTab,
+    Button,
+  },
+};
+</script>
+
+<style></style>
