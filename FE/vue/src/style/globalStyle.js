b9ce8166c440693470891c8544c19bc994704853
Hu2y <sanghun_@daum.net>
2020-06-13 01:57:37 +0900

    [#21] Style: globalstyle 추가

---
+++ FE/vue/src/style/globalStyle.js
@@ -0,0 +1,10 @@
+import styled from 'vue-styled-components';
+
+export const GlobalContainer = styled.div`
+  width: 1000px;
+  margin: 0 auto;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial,
+    sans-serif, Apple Color Emoji, Segoe UI Emoji;
+  font-size: 14px;
+  line-height: 1.5;
+`;
