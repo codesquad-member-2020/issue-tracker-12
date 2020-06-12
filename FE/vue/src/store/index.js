50e8ea345bca8a9a83660c79c3e16b37b0b4bb2d
Hu2y <sanghun_@daum.net>
2020-06-13 01:57:16 +0900

    [#21] Feat: store에 라벨 컴포넌트에 필요한 상태값 및 mutation 추가

--- FE/vue/src/store/index.js
+++ FE/vue/src/store/index.js
@@ -1,15 +1,81 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import _ from '@/utils/utils';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
+    isCreateLabel: false,
+    previewStyle: '',
+    labelName: '' || 'Label Preview',
     labelCount: 0,
     milestoneCount: 0,
     toggleColor: false,
+    labels: [
+      {
+        id: 1,
+        textColor: '#fff',
+        backgroundColor: 'rgb(203,92,208)',
+        description: 'testing label',
+        labelName: 'duplicate',
+      },
+      {
+        id: 2,
+        textColor: '#fff',
+        backgroundColor: 'rgb(254,40,119)',
+        description: 'testing label',
+        labelName: 'FE',
+      },
+      {
+        id: 3,
+        textColor: '#fff',
+        backgroundColor: 'rgb(86,185,42)',
+        description: 'testing label',
+        labelName: 'good first issue',
+      },
+      {
+        id: 4,
+        textColor: '#fff',
+        backgroundColor: 'rgb(118,148,231)',
+        description: 'testing label',
+        labelName: 'help wanted',
+      },
+      {
+        id: 5,
+        textColor: '#000',
+        backgroundColor: 'rgb(128,177,104)',
+        description: 'testing label',
+        labelName: 'question',
+      },
+    ],
   },
-  mutations: {},
+  mutations: {
+    changeLabelName(state, labelName) {
+      if (labelName == '') return (state.labelName = '' || 'Label Preview');
+      state.labelName = labelName;
+    },
+
+    setPreviewStyle(state) {
+      const previewStyle = _.pipe(
+        _.createRandomHexColor,
+        _.hexToLuma,
+        _.isWhiteColor,
+      )();
+      state.previewStyle = previewStyle;
+    },
+    visableCreateLabel(state) {
+      state.isCreateLabel = !state.isCreateLabel;
+    },
+    addToLabelList(state) {
+      console.log(state.previewStyle);
+      // const obj = {
+      //   id: state.labels.length,
+
+      // }
+      // state.labels.push(obj);
+    },
+  },
 });
 
 export default store;
