b4f1afac2fd60e6d7552517eebaec06dd30202ba
Hu2y <sanghun_@daum.net>
2020-06-13 19:24:44 +0900

    [#21] Feat: label save mutations 구현

--- FE/vue/src/store/index.js
+++ FE/vue/src/store/index.js
@@ -6,6 +6,7 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
+    isEdit: false,
     isCreateLabel: false,
     previewStyle: '',
     labelName: '' || 'Label Preview',
@@ -17,35 +18,35 @@ const store = new Vuex.Store({
       {
         id: 1,
         textColor: '#fff',
-        backgroundColor: 'rgb(203,92,208)',
+        backgroundColor: '#CB5CD0',
         description: 'testing label',
         labelName: 'duplicate',
       },
       {
         id: 2,
         textColor: '#fff',
-        backgroundColor: 'rgb(254,40,119)',
+        backgroundColor: '#FA2877',
         description: 'testing label',
         labelName: 'FE',
       },
       {
         id: 3,
         textColor: '#fff',
-        backgroundColor: 'rgb(86,185,42)',
+        backgroundColor: '#56B92A',
         description: 'testing label',
         labelName: 'good first issue',
       },
       {
         id: 4,
         textColor: '#fff',
-        backgroundColor: 'rgb(118,148,231)',
+        backgroundColor: '#7694E7',
         description: 'testing label',
         labelName: 'help wanted',
       },
       {
         id: 5,
         textColor: '#000',
-        backgroundColor: 'rgb(128,177,104)',
+        backgroundColor: '#80B168',
         description: 'testing label',
         labelName: 'question',
       },
@@ -74,13 +75,19 @@ const store = new Vuex.Store({
         ...state.previewStyle,
         ...payload,
       };
-      console.log(obj);
       state.labels.push(obj);
     },
     deleteLabel(state, labelId) {
       state.labels = state.labels.filter(
         label => label.id !== parseInt(labelId),
       );
+    },
+    editLabel(state) {
+      state.isEdit = !state.isEdit;
+    },
+    saveLabel(state, labelInfo) {
+      let target = state.labels.filter(label => label.id === labelInfo.id);
+      Object.assign(target[0], labelInfo);
     },
   },
 });
