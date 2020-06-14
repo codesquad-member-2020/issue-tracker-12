c27f79364cd4330c13624ca8b0ad29d0163b1888
Hu2y <sanghun_@daum.net>
2020-06-14 12:19:29 +0900

    [#39] Feat: milestone 헤더영역 추가

---
+++ FE/vue/src/components/issue/milestone/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <Hedaer class="headerContainer">
+    <RouterBox />
+    <CreateButton :btnTitle="'New milestone'"></CreateButton>
+  </Hedaer>
+</template>
+
+<script>
+import RouterBox from '@/components/common/RouterBox';
+import CreateButton from '@/components/common/CreateButton';
+import { Hedaer } from '@/style/styled';
+
+export default {
+  components: {
+    RouterBox,
+    CreateButton,
+    Hedaer,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
