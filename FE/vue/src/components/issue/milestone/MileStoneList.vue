b8404aea0e138b5c1d38dad288c16d3c571525cd
Hu2y <sanghun_@daum.net>
2020-06-15 19:04:05 +0900

    [#39] Feat: open 유무에 따른 마일스톤 랜더링 분기처리

---
+++ FE/vue/src/components/issue/milestone/MileStoneList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="this.$store.state.isOpenMilestone">
+    open
+  </div>
+  <div v-else>
+    close
+  </div>
+</template>
+
+<script>
+export default {
+  updated() {
+    // console.log(1);
+  },
+};
+</script>
+
+<style></style>
