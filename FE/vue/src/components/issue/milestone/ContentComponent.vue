7c4e87f0baafb177d3a8139b0d843cd100f540d4
Hu2y <sanghun_@daum.net>
2020-06-15 19:02:53 +0900

    [#39] Feat: Milestone content 컴포넌트 추가
     - 마일스톤 open/close 리스트를 보여주는 컴포넌트

---
+++ FE/vue/src/components/issue/milestone/ContentComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <fragment>
+    <LabelListWrap>
+      <InnerListWrap>
+        <MilestoneBox></MilestoneBox>
+        <MileStoneList></MileStoneList>
+      </InnerListWrap>
+    </LabelListWrap>
+  </fragment>
+</template>
+
+<script>
+import MilestoneBox from '@/components/issue/milestone/MilestoneBox';
+import MileStoneList from '@/components/issue/milestone/MileStoneList';
+import { LabelListWrap, InnerListWrap } from '@/style/styled';
+
+export default {
+  components: {
+    LabelListWrap,
+    InnerListWrap,
+    MilestoneBox,
+    MileStoneList,
+  },
+};
+</script>
+
+<style></style>
