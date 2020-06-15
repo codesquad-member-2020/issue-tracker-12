3942db038b5dfaaac69f466dab50610d906c3388
Hu2y <sanghun_@daum.net>
2020-06-15 23:58:09 +0900

    [#39] Feat: open 된 마일스톤 컴포넌트 생성
     - 마일스톤 목록중 open된 마일스톤만 가져와서 랜더링 시켜주는 컴포넌트를 만들었다.

---
+++ FE/vue/src/components/issue/milestone/OpenMilestone.vue
@@ -0,0 +1,38 @@
+<template>
+  <fragment>
+    <MilestoneItemWrap
+      class="test"
+      v-for="milestone in openMilestoneList"
+      :key="milestone.id"
+    >
+      <Milestone :milestone="milestone" />
+    </MilestoneItemWrap>
+  </fragment>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import Milestone from '@/components/issue/milestone/Milestone';
+import { MilestoneItemWrap } from '@/style/styled';
+export default {
+  data() {
+    return {
+      openMilestoneList: [],
+    };
+  },
+  created() {
+    this.openMilestoneList = this.milestonesList.filter(
+      item => item.isOpen === true,
+    );
+  },
+  computed: {
+    ...mapState(['milestonesList']),
+  },
+  components: {
+    Milestone,
+    MilestoneItemWrap,
+  },
+};
+</script>
+
+<style></style>
