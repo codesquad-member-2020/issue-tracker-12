23bfef927f5e04fc695c0db72e240dbc0f266f26
wooody92 <pkower4@naver.com>
2020-06-16 15:35:50 +0900

    [#45] feat : create user-service
    - user 전체 목록을 갖는 메서드 생성

---
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/service/UserService.java
@@ -0,0 +1,23 @@
+package dev.codesquad.issuetracker.service;
+
+import dev.codesquad.issuetracker.repository.UserRepository;
+import dev.codesquad.issuetracker.web.dto.user.UserResponse;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public List<UserResponse> viewAll() {
+        return userRepository.findAll().stream()
+            .map(user -> UserResponse.of(user))
+            .collect(Collectors.toList());
+    }
+}
