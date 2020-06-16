3d4af4f0a86eb04ec6aebf2885ee77950c873be7
wooody92 <pkower4@naver.com>
2020-06-16 16:07:28 +0900

    [#45] feat : add milestone DTO columns
    - 응답양식에 필요한 객체 추가했습니다.

--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/web/dto/milestone/MilestoneDto.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/web/dto/milestone/MilestoneDto.java
@@ -1,6 +1,8 @@
 package dev.codesquad.issuetracker.web.dto.milestone;
 
+import dev.codesquad.issuetracker.domain.Status;
 import dev.codesquad.issuetracker.domain.milestone.Milestone;
+import java.time.LocalDate;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,17 +11,26 @@ public class MilestoneDto {
 
     private Long id;
     private String title;
+    private String description;
+    private Status status;
+    private LocalDate dueDate;
 
     @Builder
-    protected MilestoneDto(Long id, String title) {
+    protected MilestoneDto(Long id, String title, String description, Status status, LocalDate dueDate) {
         this.id = id;
         this.title = title;
+        this.description = description;
+        this.status = status;
+        this.dueDate = dueDate;
     }
 
     public static MilestoneDto of(Milestone milestone) {
         return MilestoneDto.builder()
             .id(milestone.getId())
             .title(milestone.getTitle())
+            .description(milestone.getDescription())
+            .status(milestone.getStatus())
+            .dueDate(milestone.getDueDate())
             .build();
     }
 }
