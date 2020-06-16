1152029d27ac26f08f821963685380a58ab5ce23
wooody92 <pkower4@naver.com>
2020-06-16 15:35:23 +0900

    [#45] feat : add id column on response dto
    - 응답값에 id 값 추가

--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/web/dto/user/UserResponse.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/web/dto/user/UserResponse.java
@@ -7,17 +7,20 @@ import lombok.Data;
 @Data
 public class UserResponse {
 
+    private Long id;
     private String githubId;
     private String profileUrl;
 
     @Builder
-    protected UserResponse(String githubId, String profileUrl) {
+    protected UserResponse(Long id, String githubId, String profileUrl) {
+        this.id = id;
         this.githubId = githubId;
         this.profileUrl = profileUrl;
     }
 
     public static UserResponse of(User user) {
         return UserResponse.builder()
+            .id(user.getId())
             .githubId(user.getGithubId())
             .profileUrl(user.getProfileUrl())
             .build();
