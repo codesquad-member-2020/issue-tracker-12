b5e361242b6ee447a6deb4d29c5e104e0eaa0af9
wooody92 <pkower4@naver.com>
2020-06-16 20:02:59 +0900

    [#45] feat : create IssueRequest dto
    - 이슈 요청양식에 필요한 dto 생성

--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/web/dto/issue/IssueRequest.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/web/dto/issue/IssueRequest.java
@@ -1,5 +1,7 @@
 package dev.codesquad.issuetracker.web.dto.issue;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -11,4 +13,13 @@ public class IssueRequest {
 
     @NotNull
     private String content;
+
+    @NotNull
+    private List<Long> userId = new ArrayList<>();
+
+    @NotNull
+    private List<Long> labelId = new ArrayList<>();
+
+    @NotNull
+    private Long milestoneId;
 }
