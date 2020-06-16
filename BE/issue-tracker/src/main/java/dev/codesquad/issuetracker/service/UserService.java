dc3e4a18607798412f0bd926ecd21047534f67dd
wooody92 <pkower4@naver.com>
2020-06-16 15:48:41 +0900

    [#45] feat : change user list response
    - 응답 양식 통일을 위해 ResultDto로 변경

--- BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/service/UserService.java
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/service/UserService.java
@@ -1,6 +1,7 @@
 package dev.codesquad.issuetracker.service;
 
 import dev.codesquad.issuetracker.repository.UserRepository;
+import dev.codesquad.issuetracker.web.dto.ResultDto;
 import dev.codesquad.issuetracker.web.dto.user.UserResponse;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,7 +16,13 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
-    public List<UserResponse> viewAll() {
+    public ResultDto viewAll() {
+        List<UserResponse> userResponses = getUserResponses();
+        return new ResultDto(userResponses.size(), userResponses);
+    }
+
+    @Transactional(readOnly = true)
+    public List<UserResponse> getUserResponses() {
         return userRepository.findAll().stream()
             .map(user -> UserResponse.of(user))
             .collect(Collectors.toList());
