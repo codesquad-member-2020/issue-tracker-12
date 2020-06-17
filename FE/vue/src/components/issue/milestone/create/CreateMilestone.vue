0e8fc991869361bc344f5bd2d01827552f42dada
Hu2y <sanghun_@daum.net>
2020-06-17 18:38:57 +0900

    [#44] Feat: 마일스톤 추가하는 컴포넌트 생성

---
+++ FE/vue/src/components/issue/milestone/create/CreateMilestone.vue
@@ -0,0 +1,25 @@
+<template>
+  <fragment>
+    <CreateMilestoneHeader></CreateMilestoneHeader>
+    <CreateMilestoneContent :propsData="milestoneData"></CreateMilestoneContent>
+  </fragment>
+</template>
+
+<script>
+import CreateMilestoneHeader from '@/components/issue/milestone/create/CreateMilestoneHeader';
+import CreateMilestoneContent from '@/components/issue/milestone/create/CreateMilestoneContent';
+export default {
+  data() {
+    return {
+      milestoneData: [],
+    };
+  },
+  created() {},
+  components: {
+    CreateMilestoneHeader,
+    CreateMilestoneContent,
+  },
+};
+</script>
+
+<style></style>
