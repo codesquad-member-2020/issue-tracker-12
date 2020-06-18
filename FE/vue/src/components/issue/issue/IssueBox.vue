bfdf80297eef70c579e53652bad3001f76621926
Hu2y <sanghun_@daum.net>
2020-06-18 20:24:38 +0900

    [#55] Feat: Issue에 헤더에 필요한 버튼링크 추가

---
+++ FE/vue/src/components/issue/issue/IssueBox.vue
@@ -0,0 +1,49 @@
+<template>
+  <ListTopMenu>
+    <BtnWrapper class="btnWrapper">
+      <ItemWrapper>
+        <input type="checkbox" />
+        <StateIssueButton :type="open" />
+        <StateIssueButton :type="close" />
+      </ItemWrapper>
+      <ItemWrapper>
+        <FilterItem :filterTitle="'Author'" />
+      </ItemWrapper>
+    </BtnWrapper>
+  </ListTopMenu>
+</template>
+
+<script>
+import { ListTopMenu, BtnWrapper, ItemWrapper } from '@/style/styled';
+import StateIssueButton from '@/components/issue/issue/StateIssueButton';
+import FilterItem from '@/components/issue/issue/FilterItem';
+
+export default {
+  data() {
+    return {
+      open: 'open',
+      close: 'close',
+    };
+  },
+  components: {
+    ListTopMenu,
+    BtnWrapper,
+    StateIssueButton,
+    ItemWrapper,
+    FilterItem,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btnWrapper {
+  color: #586069;
+  font-weight: 400;
+  width: 100%;
+  justify-content: space-between;
+  input {
+    margin-top: 4px;
+    margin-right: 15px;
+  }
+}
+</style>
