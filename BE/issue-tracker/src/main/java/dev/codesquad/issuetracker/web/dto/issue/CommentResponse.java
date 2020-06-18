b43ea0735d1b1541f3b9b37ead813820bdf88b5f
wooody92 <pkower4@naver.com>
2020-06-18 19:48:06 +0900

    [#53] feat : create comment response dto
    - 코멘트 응답 dto 생성

---
+++ BE/issue-tracker/src/main/java/dev/codesquad/issuetracker/web/dto/issue/CommentResponse.java
@@ -0,0 +1,29 @@
+package dev.codesquad.issuetracker.web.dto.issue;
+
+import dev.codesquad.issuetracker.domain.issue.Comment;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class CommentResponse {
+
+    private Long id;
+    private String content;
+    private LocalDate createTime;
+
+    @Builder
+    protected CommentResponse(Long id, String content, LocalDate createTime) {
+        this.id = id;
+        this.content = content;
+        this.createTime = createTime;
+    }
+
+    public static CommentResponse of(Comment comment) {
+        return CommentResponse.builder()
+            .id(comment.getId())
+            .content(comment.getContent())
+            .createTime(comment.getCreate_time())
+            .build();
+    }
+}
