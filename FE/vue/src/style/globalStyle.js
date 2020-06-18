6557f3f83c291011585cbae1c93a07e5f1507488
Hu2y <sanghun_@daum.net>
2020-06-18 17:37:49 +0900

    [#55] Style: global스타일에 padding추가

--- FE/vue/src/style/globalStyle.js
+++ FE/vue/src/style/globalStyle.js
@@ -2,6 +2,7 @@ import styled from 'vue-styled-components';
 
 export const GlobalContainer = styled.div`
   width: 1000px;
+  padding: 0px 16px;
   margin: 0 auto;
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial,
     sans-serif, Apple Color Emoji, Segoe UI Emoji;
