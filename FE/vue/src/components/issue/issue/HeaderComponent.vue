aae1d02be32ece9ebf71ce51dda9137288530d6e
Hu2y <sanghun_@daum.net>
2020-06-18 17:39:29 +0900

    [#55] Feat: Issue 헤더 컴포넌트 생성

---
+++ FE/vue/src/components/issue/issue/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <Hedaer>
+    <FilterComponent></FilterComponent>
+    <div class="btn-box">
+      <RouterBox></RouterBox>
+      <CreateButton :btnTitle="'New issue'"></CreateButton>
+    </div>
+  </Hedaer>
+</template>
+
+<script>
+import { Hedaer } from '@/style/styled';
+import FilterComponent from '@/components/issue/issue/FilterComponent';
+import RouterBox from '@/components/common/RouterBox';
+import CreateButton from '@/components/common/CreateButton';
+export default {
+  components: {
+    Hedaer,
+    FilterComponent,
+    RouterBox,
+    CreateButton,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btn-box {
+  display: flex;
+}
+</style>
